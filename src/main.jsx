b0202b3019cc95bb4d93b72ff71626c44b0d1092
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-03-20 14:29:41 -0300

    created styles/theme.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+
 import App from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
