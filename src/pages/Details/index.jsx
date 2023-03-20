cea737e98bf27f5bc0b2458d8028cb36060baa68
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-03-20 18:08:55 -0300

    completed pages/Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -4,23 +4,42 @@ import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
 import { Button } from '../../components/Button'
 
-import { Container } from './styles'
+import { Container, Links, Content } from './styles'
 
 export function Details() {
   return (
     <Container>
       <Header/>
-      <TextButton title = 'Delete Note'/>
-      <Section title = 'Helpful Links'>
-        <a>dd</a>
-      </Section>
-      <Section title = 'Tags'>
-        <div id="tags">
-        <Tag title={'express'}/>
-        <Tag title={'node.js'}/>
-        </div>
-      </Section>
-      <Button title = 'Return'/>
+
+      <main>
+        <Content>
+          <TextButton title = 'Delete Note'/>
+          <h1>Gettings Started - React</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Debitis eveniet ipsum iure deserunt modi, asperiores, 
+            nihil perferendis inventore ea repudiandae in. 
+            Eius ab nemo natus error iste sapiente expedita perspiciatis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Debitis eveniet ipsum iure deserunt modi, asperiores, 
+            nihil perferendis inventore ea repudiandae in. 
+            Eius ab nemo natus error iste sapiente expedita perspiciatis.
+          </p>
+          <Section title = 'Links'>
+            <Links>
+              <li> <a href='#'>https://www.rocketseat.com.br/</a></li>
+              <li> <a href='#'>https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+          <Section title = 'Tags'>
+            <div id="tags">
+            <Tag title={'express'}/>
+            <Tag title={'node.js'}/>
+            </div>
+          </Section>
+          <Button title = 'Return'/>
+        </Content>
+      </main>
     </Container>
   )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,5 +1,58 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
 
+  display: grid;
+  grid-template-areas: "header" "content";
+  grid-template-rows: 10.6rem auto;
+
+  >main {
+    grid-area: content;
+    overflow-y: auto;
+    padding: 6.4rem 0;
+  }
+`
+export const Content = styled.div`
+  width: 55rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  >button:first-child {
+    align-self: end;
+    margin-bottom: 6.4rem;
+  }
+
+  >h1 {
+    font-weight: 500;
+    font-size: 3.6rem;
+    line-height: 4.7rem;
+    margin-bottom: 1.6rem;
+  }
+
+  >p {
+    line-height: 1.9rem;
+    text-align: justify;
+    margin-bottom: 3.8rem;
+  }
+
+  >button:last-child {
+    margin-top: 11.3rem;
+  }
+`
+
+export const Links = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  gap: 1.2rem;
+
+  li {
+    a {
+      line-height: 1.9rem;
+      color: ${ ({theme}) => theme.COLORS.WHITE}
+    }
+  }
 `
