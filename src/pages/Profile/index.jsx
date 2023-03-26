0a1890a60b2cf017aa0696dc3dfbcfd9e25019fa
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-03-26 10:15:49 -0300

    created pages/Profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,38 @@
+import { FiArrowLeft, FiCamera, FiUser, FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+ 
+import { Container, Form, Picture } from './styles'
+
+export function Profile () {
+  return (
+    <Container>
+      <header>
+        <button type='button'> <FiArrowLeft/> </button>
+      </header>
+
+      <main>
+        <Form>
+          <Picture>
+            <img
+              src = 'https://github.com/tarcisioMateus.png'
+              alt = "Picture from user"/>
+            
+            <label htmlFor='choose picture'>
+              <FiCamera/>
+              <input type='file' id='choose picture' className='sr-only'></input>
+            </label>
+          </Picture>
+
+          <Input placeholder='Name' label='Name' icon={FiUser}/>
+          <Input type='email' placeholder='E-mail' label='E-mail' icon={FiMail}/>
+          <Input type='password' placeholder='Current Password' label='Current Password' icon={FiLock}/>
+          <Input type='password' placeholder='New Password' label='New Password' icon={FiLock}/>
+          <Button title='save'/>
+        </Form>
+      </main>
+      
+    </Container>
+  )
+}
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  >header {
+    height: 14.4rem;
+    background-color: ${ ({theme}) => theme.COLORS.BACKGROUND_900 };
+    padding-left: 14.4rem;
+
+    display: flex;
+    align-items: center;
+
+    >button {
+      background: none;
+      border: none;
+     
+      >svg {
+        font-size: 2rem;
+      }
+    }
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin: 0 auto;
+  margin-top: -9.3rem;
+  width: 34.8rem;
+
+  >div {
+    margin-bottom: 0.8rem;
+  }
+
+  >div:first-of-type {
+    margin-bottom: 6.4rem;
+  }
+
+  >div:nth-of-type(3) {
+    margin-bottom: 2.4rem;
+  }
+
+  >button:last-of-type {
+    margin-top: 1.6rem;
+    margin-bottom: 20rem;
+  }
+`
+
+export const Picture = styled.div`
+  width: 18.6rem;
+  height: 18.6rem;
+  position: relative;
+
+  >img {
+    width: 18.6rem;
+    height: 18.6rem;
+    border-radius: 50%;
+  }
+
+  >label {
+    width: 4.8rem;
+    height: 4.8rem;
+    border-radius: 50%;
+    background-color: ${ ({theme}) => theme.COLORS.ORANGE };
+
+    position: absolute;
+    right: 0.4rem;
+    bottom: 0.4rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    >svg {
+      width: 2rem;
+      height: 2rem;
+
+      color: ${ ({theme}) => theme.COLORS.BACKGROUND_800 };
+    }
+  }
+
+`
