fd07d3be04ab13434f02ddcf86c7b3e002bad85b
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-26 20:29:31 -0300

    created hooks/auth.jsx

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,7 +6,6 @@ import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { TextButton } from '../../components/TextButton'
 
 import { Container, Form, Header, Img} from "./styles";
 
