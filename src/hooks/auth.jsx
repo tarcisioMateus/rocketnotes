5710c4a6b3337aa0c598e40eb00ee4343dac301b
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-26 20:46:09 -0300

    signIn() & user provided to the AuthContext

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,32 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState } from 'react'
+import { api } from '../services'
 
 const AuthContext = createContext({})
 
 function AuthProvider( { children } ) {
+  const [data, setData] = useState({})
+
+  async function signIn({email, password}) {
+    try {
+      const response = await api.post('/sessions', {email, password})
+      const { token, user } = response.data
+
+      api.defaults.headers.common['Authorization'] = ` Beare ${token}`
+      localStorage.setItem('@rocketNotes:token', token)
+      localStorage.setItem('@rocketNotes:user', JSON.stringify(user))
+      setData({ token, user })
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('unable to create account')
+      }
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{ signIn, user: data.user}}>
       {children}
     </AuthContext.Provider>
   )
